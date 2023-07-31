f534a091d68d17d7be058585ccf5f6b57e9889c2
eldarlrd <eldarlrd@pm.me>
2023-07-31 17:13:43 +0400

    chore: Twind classes

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,3 @@
-export function App() {
+export const App = () => {
   return <>Hello World!</>;
-}
+};
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,5 @@ import config from '../twind.config.mts';
 import { App } from '@/app.tsx';
 
 install(config, import.meta.env.PROD);
-
 const appDiv = document.getElementById('app');
 if (appDiv) render(<App />, appDiv);
