d4979e935604e55541a65f394e9c5bbe5284dad9
eldarlrd <eldarlrd@pm.me>
2023-08-02 14:33:58 +0400

    refactor: Component structure

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,15 @@
+import { Header } from '@/components/header.tsx';
+import { Navbar } from '@/components/navbar.tsx';
+import { List } from '@/components/list.tsx';
+import { Footer } from '@/components/footer.tsx';
+
 export const App = () => {
-  return <>Hello World!</>;
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <List />
+      <Footer />
+    </>
+  );
 };
