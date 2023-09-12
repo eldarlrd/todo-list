369a55d590835d77f9aabd633f39cee575922e5e
eldarlrd <eldarlrd@pm.me>
2023-09-13 01:41:02 +0400

    feat: Add Todo

--- src/components/controls/controlPanel.tsx
+++ src/components/controls/controlPanel.tsx
@@ -1,30 +1,51 @@
 import { Plus } from 'lucide-preact';
+import { useState } from 'preact/hooks';
 import { type JSX } from 'preact/jsx-runtime';
 
 import { SortMenu } from '@/components/controls/sortMenu.tsx';
 import { ViewMenu } from '@/components/controls/viewMenu.tsx';
+import { AddTodo } from '@/components/modals/addTodo.tsx';
+import { ModalWindow } from '@/components/modals/modalWindow.tsx';
+import { useVisible } from '@/hooks/useVisible.ts';
 
 export const ControlPanel = (): JSX.Element => {
+  const [modalContent, setModalContent] = useState<JSX.Element>();
+  const { ref, isVisible, setIsVisible } = useVisible(false);
+
   return (
-    <div
-      id='control-panel'
-      class='dark:(bg-slate-600, text-slate-50) flex min-h-[2.5rem] items-center justify-between bg-slate-300 px-3 py-1 text-slate-900 transition-all md:py-1.5'>
-      <button
-        id='add-todo'
-        type='button'
-        class='hover:(bg-violet-700, active:bg-violet-600, dark:(bg-pink-700, active:bg-pink-800)) md:(max-w-[8.5rem], text-lg, gap-1, pl-3, pr-4, py-1.5) flex max-w-[7.25rem] items-center gap-0.5 whitespace-nowrap break-all rounded-md bg-violet-800 py-1 pl-2 pr-3 font-medium leading-4 text-white transition-all dark:bg-pink-600'>
-        <Plus
-          aria-label='Plus Sign'
-          strokeWidth='2.25'
-          size='22'
-          class='md:scale-110'
-        />
-        Add Todo
-      </button>
-      <div class='flex gap-2'>
-        <SortMenu />
-        <ViewMenu />
+    <>
+      <div
+        id='control-panel'
+        class='dark:(bg-slate-600, text-slate-50) flex min-h-[2.5rem] items-center justify-between bg-slate-300 px-3 py-1 text-slate-900 transition-all md:py-1.5'>
+        <button
+          id='add-todo'
+          type='button'
+          onClick={(): void => {
+            setIsVisible(true);
+            setModalContent(
+              <AddTodo key='Add Todo' setIsVisible={setIsVisible} />
+            );
+          }}
+          class='hover:(bg-violet-700, active:bg-violet-600, dark:(bg-pink-700, active:bg-pink-800)) md:(max-w-[8.5rem], text-lg, gap-1, pl-3, pr-4, py-1.5) flex max-w-[7.25rem] items-center gap-0.5 whitespace-nowrap break-all rounded-md bg-violet-800 py-1 pl-2 pr-3 font-medium leading-4 text-white transition-all dark:bg-pink-600'>
+          <Plus
+            aria-label='Plus Sign'
+            strokeWidth='2.25'
+            size='22'
+            class='md:scale-110'
+          />
+          Add Todo
+        </button>
+        <div class='flex gap-2'>
+          <SortMenu />
+          <ViewMenu />
+        </div>
       </div>
-    </div>
+      <ModalWindow
+        modalContent={modalContent}
+        setIsVisible={setIsVisible}
+        isVisible={isVisible}
+        refer={ref}
+      />
+    </>
   );
 };
---
+++ src/components/modals/addTodo.tsx
@@ -0,0 +1,19 @@
+import { type StateUpdater } from 'preact/hooks';
+import { type JSX } from 'preact/jsx-runtime';
+
+export const AddTodo = ({
+  setIsVisible
+}: {
+  setIsVisible: StateUpdater<boolean>;
+}): JSX.Element => {
+  return (
+    <div>
+      Todo
+      <button
+        onClick={(): void => {
+          setIsVisible(false);
+        }}
+      />
+    </div>
+  );
+};
