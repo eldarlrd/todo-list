a69d299cca71f970d4d11ece72f4df70f58b9957
eldarlrd <eldarlrd@pm.me>
2023-09-19 20:05:44 +0400

    refactor: Prop order

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -94,6 +94,7 @@ const SidePanel = ({
             <button
               type='button'
               title='Delete Project'
+              class='hover:(text-rose-900, dark:text-rose-400) duration-150'
               onClick={(): void => {
                 setIsVisible(true);
                 setModalContent(
