6afdaf8114bd340de620ca22c4e054cffc34afb1
eldarlrd <eldarlrd@pm.me>
2023-09-21 22:30:02 +0400

    design: Delete & Cancel

--- src/components/modals/addProject.tsx
+++ src/components/modals/addProject.tsx
@@ -137,7 +137,7 @@ export const AddProject = ({
         <button
           type='button'
           id='project-cancel'
-          class='hover:(bg-rose-700, border-rose-700, text-slate-50, active:(bg-rose-800, border-rose-800)) border-1 md:(w-24, py-2) xl:(w-32, py-2.5, text-xl) max-h-[3rem] w-20 rounded-md border-current py-1.5 text-lg font-medium text-rose-700 transition-all dark:text-slate-50 lg:w-28'
+          class='hover:(bg-slate-100, active:(bg-slate-50), dark:(bg-slate-800, active:bg-slate-900)) md:(w-24, py-2) xl:(w-32, py-2.5, text-xl) max-h-[3rem] w-20 rounded-md py-1.5 text-lg font-medium text-slate-900 transition-all dark:text-slate-50 lg:w-28'
           onClick={(): void => {
             setIsVisible(false);
           }}>
--- src/components/modals/deleteModal.tsx
+++ src/components/modals/deleteModal.tsx
@@ -9,11 +9,11 @@ export const DeleteModal = ({
   return (
     <div class='flex flex-col'>
       Delete?
-      <div class='flex items-center justify-between'>
+      <div class='flex justify-between pt-1.5'>
         <button
           type='button'
           id='delete-cancel'
-          class=''
+          class='hover:(bg-slate-100, active:(bg-slate-50), dark:(bg-slate-800, active:bg-slate-900)) md:(w-24, py-2) xl:(w-32, py-2.5, text-xl) max-h-[3rem] w-20 rounded-md py-1.5 text-lg font-medium text-slate-900 transition-all dark:text-slate-50 lg:w-28'
           onClick={(): void => {
             setIsVisible(false);
           }}>
@@ -22,7 +22,7 @@ export const DeleteModal = ({
         <button
           type='button'
           id='delete-item'
-          class='hover:(bg-rose-700, border-rose-700, text-slate-50, active:(bg-rose-800, border-rose-800)) border-1 md:(w-24, py-2) xl:(w-32, py-2.5, text-xl) max-h-[3rem] w-20 rounded-md border-current py-1.5 text-lg font-medium text-rose-700 transition-all dark:text-slate-50 lg:w-28'
+          class='hover:(bg-rose-700, active:bg-rose-600, dark:(bg-rose-700, active:bg-rose-800)) md:(w-24, py-2) xl:(w-32, py-2.5, text-xl) max-h-[3rem] w-20 rounded-md dark:bg-rose-600 bg-rose-800 py-1.5 text-lg font-medium text-white transition-all lg:w-28'
           onClick={(): void => {
             setIsVisible(false);
           }}>
