1ff741725b1efb6889eefd09d8fb07e2dab658d7
eldarlrd <eldarlrd@pm.me>
2023-10-21 19:53:43 +0400

    update: Dependencies

--- src/components/controls/userSignIn.tsx
+++ src/components/controls/userSignIn.tsx
@@ -5,10 +5,10 @@ export const UserSignIn = (): JSX.Element => {
   return (
     <button
       type='button'
-      title='Sign in'
+      title='Sign In'
       id='user-sign-in'
       onClick={(): void => {
-        console.log('Sign in');
+        console.log('Sign In');
       }}
       class='hover:(bg-slate-200, active:bg-slate-300, dark:(bg-slate-700, active:bg-slate-600)) lg:(w-14, px-4, py-3) xl:(w-16, px-5, py-4) w-12 rounded px-3 py-2 leading-4 transition-colors'>
       <User2
