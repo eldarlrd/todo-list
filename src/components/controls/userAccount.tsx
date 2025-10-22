dfb623f3b7940bf5b4a944faf37aa027a8fc5d7c
eldarlrd <eldarlrd@pm.me>
2025-10-22 16:43:56 +0400

    feat(auth): account icon swap

--- src/components/controls/userAccount.tsx
+++ src/components/controls/userAccount.tsx
@@ -1,12 +1,29 @@
-import { User2 } from 'lucide-preact';
+import { UserRound, UserRoundCheck } from 'lucide-preact';
 import { type JSX } from 'preact/jsx-runtime';
 
 import { ModalWindow } from '@/components/modals/modalWindow.tsx';
 import { UserModal } from '@/components/modals/userModal.tsx';
+import { useAppSelector } from '@/hooks/useAppState.ts';
 import { useVisible } from '@/hooks/useVisible.ts';
+
+const USER_ICONS: JSX.Element[] = [
+  <UserRound
+    key='user'
+    aria-label='User'
+    strokeWidth='2.25'
+    class='-ml-px scale-110 transition-transform lg:scale-125 xl:scale-150'
+  />,
+  <UserRoundCheck
+    key='user-check'
+    aria-label='User Check'
+    strokeWidth='2.25'
+    class='ml-0.5 scale-110 transition-transform lg:scale-125 xl:scale-150'
+  />
+];
 
 export const UserAccount = (): JSX.Element => {
   const { refer, isVisible, setIsVisible } = useVisible(false);
+  const { user } = useAppSelector(state => state.authReducer);
 
   return (
     <div>
@@ -18,11 +35,7 @@ export const UserAccount = (): JSX.Element => {
           setIsVisible(true);
         }}
         class='hover:(bg-slate-200, active:bg-slate-300, dark:(bg-slate-700, active:bg-slate-600)) lg:(w-14, px-4, py-3) xl:(w-16, px-5, py-4) w-12 rounded px-3 py-2 leading-4 transition-colors'>
-        <User2
-          aria-label='User'
-          strokeWidth='2.25'
-          class='scale-110 transition-transform lg:scale-125 xl:scale-150'
-        />
+        {user ? USER_ICONS[1] : USER_ICONS[0]}
       </button>
 
       <ModalWindow
